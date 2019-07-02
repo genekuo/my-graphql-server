f857359e5e5f60cdb14955136fb1e2a52c552952
KuoYu-chiung <genekuo@cybersoft4u.com>
2019-07-02 19:24:25 +0800

    Add more queries and mutations

--- src/index.js
+++ src/index.js
@@ -1,23 +1,122 @@
+import cors from 'cors';
 import express from 'express';
 import { ApolloServer, gql } from 'apollo-server-express';
+import uuidv4 from 'uuid/v4';
 
 const app = express();
+app.use(cors())
+
 const schema = gql`
     type Query {
-        user: User
+        signin: User
+        user(id: ID!): User
+        users: [User!]
+
+        orders: [Order!]!
+        order(id: ID!): Order!
+    }
+
+    type Mutation {
+        placeOrder(product: String!): Order!
+        deleteOrder(id: ID!): Boolean!
     }
 
     type User {
-        username: String!
+        id: ID!
+        username: String!,
+        orders: [Order!]
+    }
+
+    type Order {
+        id: ID!
+        product: String!
+        user: User!
     }
 `;
 
+let users = {
+    1: {
+        id: '1',
+        firstname: 'Gene',
+        lastname: 'Kuo',
+        orderIds: [1],
+    },
+    2: {
+        id: '2',
+        firstname: 'Barry',
+        lastname: 'Jan',
+        orderIds: [2],
+    },
+};
+
+let orders = {
+    1: {
+        id: '1',
+        product: 'iMac',
+        userId: '1'
+    },
+    2: {
+        id: '2',
+        product: 'MacBook Pro',
+        userId: '2',
+    },
+};
+
+//const signin = users[1];
+
 const resolvers = {
     Query: {
-        user: () => {
-            return {
-                username: 'Gene Kuo'
+        signin: (parent, args, { me }) => {
+            return me;
+        },
+        user: (parent, { id }) => {
+            return users[id];
+        },
+        users: () => {
+            return Object.values(users);
+        },
+        orders: () => {
+            return Object.values(orders);
+        },
+        order: (parent, { id }) => {
+            return orders[id];
+        },
+    },
+
+    Mutation: {
+        placeOrder: (parent, { product }, { me }) => {
+            const id = uuidv4();
+            const order = {
+                id,
+                product,
+                userId: me.id,
             };
+            orders[id] = order;
+            users[me.id].orderIds.push(id);
+            return order;
+        },
+        deleteOrder: (parent, { id }) => {
+            const { [id]: order, ...otherOrders } = orders;
+
+            if (!order) {
+                return false;
+            }
+
+            orders = otherOrders;
+            return true;
+        },
+    },
+
+    User: {
+        username: user => `${user.firstname} ${user.lastname}`,
+        orders: user => {
+            return Object.values(orders).filter(order => order.userId === user.id);
+        },
+    },
+
+    Order: {
+        user: order => {
+            return users[order.userId];
         },
     },
 };
@@ -25,13 +124,16 @@ const resolvers = {
 const server = new ApolloServer({
     typeDefs: schema,
     resolvers,
+    context: {
+        me: users[1],
+    },
 });
 
 server.applyMiddleware({
     app,
-    path: '/my-qraphql'
+    path: '/my-graphql'
 });
 
 app.listen({ port: 8000 }, () => {
-    console.log('My Apollo server listening on http://localhost:8000/my-qraphql')
-})
+    console.log('My Apollo server listening on http://localhost:8000/my-graphql');
+});
